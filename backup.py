10f016ce6cbae92234da42e11e8b3d7bb5a5e60b
chrisyang2003 <63803385+chrisyang2003@users.noreply.github.com>
2020-05-01 23:33:27 +0800

    Update backup.py

--- backup.py
+++ backup.py
@@ -7,6 +7,7 @@ import time
 def backup():
     localtime = time.localtime(time.time())
     foder_name = to_folder + '\\' + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+    
     os.mkdir(foder_name)
     os.system('copy ' + from_folder + ' ' + foder_name)
 
